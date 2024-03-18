64992e572a74a005e19b45fd3cf4a4778f8bd11f
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2024-03-18 06:39:36 -0500

    feat: funcion de copiar valor de la contraseña

--- lib/actividad_generador_clave/widgets/password_view.dart
+++ lib/actividad_generador_clave/widgets/password_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class PasswordView extends StatelessWidget {
   late double width;
@@ -10,10 +11,10 @@ class PasswordView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
-    return _passwordView();
+    return _passwordView(context);
   }
 
-  Widget _passwordView() {
+  Widget _passwordView(BuildContext context) {
     return Container(
       width: width * 0.8,
       height: 80,
@@ -36,18 +37,30 @@ class PasswordView extends StatelessWidget {
                   style: const TextStyle(fontSize: 15, color: Colors.black),
                   textAlign: TextAlign.left),
             ),
-            _buttonGroup()
+            _buttonGroup(context)
           ]),
     );
   }
 
-  Widget _buttonGroup() {
+  Widget _buttonGroup(BuildContext context) {
     return Row(
       children: [
-        const IconButton(onPressed: null, icon: Icon(Icons.copy)),
+        IconButton(
+            onPressed: () => clip(context), icon: const Icon(Icons.copy)),
         IconButton(
             icon: const Icon(Icons.refresh), onPressed: () => onRefresh())
       ],
     );
+  }
+
+  void clip(BuildContext context) async {
+    await Clipboard.setData(ClipboardData(text: currentPassword))
+        .then((result) {
+      final snackBar = SnackBar(
+        content: const Center(child: Text('Copiado')),
+        action: SnackBarAction(label: 'Ok', onPressed: () {}),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    });
   }
 }
